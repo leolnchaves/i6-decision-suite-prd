311229182310c10676d27035ba593d0c72545afd
leolnchaves <leolnchaves@users.noreply.github.com>
2026-09-10 20:58:04 +0000

    deploy: cb4df83f96578c483ca34cc80fe0bdb16dd9f4a4

---
+++ assets/routes-CIsXZTUU.js
@@ -0,0 +1,1 @@
+import{j as e,l as t}from"./index-pWV6GU6X.js";import{t as n}from"./landing-page-D6KFGjV7.js";var r=e();function i(){let{signInHref:e}=t.useLoaderData();return(0,r.jsx)(n,{signInHref:e})}export{i as component};
