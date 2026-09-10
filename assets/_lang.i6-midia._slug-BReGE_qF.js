29025d9083a8c2ad2f1a72158ad04291d7688e21
leolnchaves <leolnchaves@users.noreply.github.com>
2026-09-10 05:04:01 +0000

    deploy: 890d2fe9a86f56a590157d1c881d3968592c55dd

---
+++ assets/_lang.i6-midia._slug-BReGE_qF.js
@@ -0,0 +1,1 @@
+import{c as e}from"./useStore-B8qJHGaJ.js";import{n as t}from"./index-AXOepUE3.js";import{t as n}from"./blog-article-page-BUvYORn9.js";var r=e();function i(){let{article:e,lang:i,signInHref:a}=t.useLoaderData();return(0,r.jsx)(n,{article:e,lang:i,signInHref:a,variant:`media`})}export{i as component};
--- assets/route-DHclCeHx.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./useStore-B8qJHGaJ.js";import{_ as t}from"./index-CP7g7zna.js";var n=e(),r=()=>(0,n.jsx)(t,{});export{r as component};
