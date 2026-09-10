eec8cc488a60406d415155541fc14e2828004375
leolnchaves <leolnchaves@users.noreply.github.com>
2026-09-10 04:28:44 +0000

    deploy: 755dd0b3489e9eb9a7dbb854d8d7d2aa6013f368

--- assets/_lang.i6-blog._slug-CTftNmvS.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./useStore-B8qJHGaJ.js";import{i as t}from"./index-xw9gdVXO.js";import{t as n}from"./blog-article-page-Nj4-joZD.js";var r=e();function i(){let{article:e,lang:i,signInHref:a}=t.useLoaderData();return(0,r.jsx)(n,{article:e,lang:i,signInHref:a,variant:`blog`})}export{i as component};
---
+++ assets/route-D5ndd5_b.js
@@ -0,0 +1,1 @@
+import{c as e}from"./useStore-B8qJHGaJ.js";import{_ as t}from"./index-HyI9WCSl.js";var n=e(),r=()=>(0,n.jsx)(t,{});export{r as component};
