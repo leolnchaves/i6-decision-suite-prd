f2ea00265effad5ede7e627fefe02b9a6a0f244c
leolnchaves <leolnchaves@users.noreply.github.com>
2026-09-17 00:12:12 +0000

    deploy: a65a6380b6cf14270f2bca85165670e407620ae1

---
+++ assets/_lang.i6-midia._slug-CGYKOXEE.js
@@ -0,0 +1,1 @@
+import{L as e}from"./preload-helper-BNKeUjY8.js";import{a as t}from"./index-Bznwy3AS.js";import{t as n}from"./blog-article-page-DujdckOD.js";var r=e();function i(){let{article:e,lang:i,signInHref:a}=t.useLoaderData();return(0,r.jsx)(n,{article:e,lang:i,signInHref:a,variant:`media`})}export{i as component};
